3a4133acd920fe5b2176f1ab1473d9202dbd1d69
NAGARRO\rishiyadav <rishi.yadav@nagarro.com>
2026-04-08 12:03:42 +0530

    change in navbar-test CI-CD

--- demo-frontend/src/Components/Navbar.js
+++ demo-frontend/src/Components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
   return (
     <div className="navbar">
       <Link to="/">Login</Link>
-      <Link to="/signup">Signup</Link>
+      <Link to="/signup">SignupPP</Link>
       <Link to="/products">Products</Link>
     </div>
   );
